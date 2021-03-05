6ead6cecd150f51d4465c56d17126af02ed9c385
Larry Jones <larry.jones@reveal-energy.com>
2021-03-05 14:22:57 -0600

    Modules also support keyword function form

--- math.exs
+++ math.exs
@@ -1,11 +1,6 @@
 defmodule Math do
-  def zero?(0) do
-    true
-  end
-
-  def zero?(x) when is_integer(x) do
-    false
-  end
+  def zero?(0), do: true
+  def zero?(x) when is_integer(x), do: false
 end
 
 IO.puts Math.zero?(0)  #=> true

b894e5e9b650081b750ca1b87ce07da8344e4fa8
Larry Jones <larry.jones@reveal-energy.com>
2021-03-05 20:24:40 -0600

    "Hand-rolled" implementation of map

--- math.exs
+++ math.exs
@@ -1,11 +1,11 @@
 defmodule Math do
-  def sum_list([head | tail], accumulator) do
-    sum_list(tail, accumulator + head)
+  def double_each([head | tail]) do
+    [head * 2 | double_each(tail)]
   end
 
-  def sum_list([], accumulator) do
-    accumulator
+  def double_each([]) do
+    []
   end
 end
 
-IO.puts Math.sum_list([1, 2, 3], 0)
+IO.inspect Math.double_each([1, 2, 3])  #=> [2, 4, 6]

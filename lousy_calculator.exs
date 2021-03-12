119f253c44b641e61eab24081ddda4d09ea39f7b
Larry Jones <larry.jones@reveal-energy.com>
2021-03-12 01:32:36 -0600

    Defining complex types and specs

---
+++ lousy_calculator.exs
@@ -0,0 +1,7 @@
+defmodule LousyCalculator do
+  @spec add(number, number) :: {number, String.t}
+  def add(x, y), do: {x + y, "You need a calculator to do that!"}
+
+  @spec multiply(number, number) :: {number, String.t}
+  def multiply(x, y), do: {x * y, "Come on, man!"}
+end

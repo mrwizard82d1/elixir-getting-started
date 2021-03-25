5d3fbe6acbbe1a0e956a05fcc456815bef800e5e
Larry Jones <larry.jones@reveal-energy.com>
2021-03-25 09:41:21 -0500

    Typedocs and custom types

--- lousy_calculator.exs
+++ lousy_calculator.exs
@@ -1,4 +1,10 @@
 defmodule LousyCalculator do
+  @typedoc """
+  Just a number followed by a string.
+  """
+
+  @type number_with_remark :: {number, String.t}
+
   @spec add(number, number) :: {number, String.t}
   def add(x, y), do: {x + y, "You need a calculator to do that!"}
 

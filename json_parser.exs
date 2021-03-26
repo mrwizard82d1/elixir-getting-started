e764be56bd2f3f3975498bbb3ade44784c4088b7
Larry Jones <larry.jones@reveal-energy.com>
2021-03-26 09:23:17 -0500

    Implement Parser behaviour

---
+++ json_parser.exs
@@ -0,0 +1,9 @@
+defmodule JsonParser do
+  @behaviour Parser
+
+  @impl Parser
+  def parse(str), do: {:ok, "some json" <> str}  # parse JSON
+
+  @impl Parser
+  def extensions, do: ["json"]
+end
---
+++ yaml_parser.exs
@@ -0,0 +1,9 @@
+defmodule YamlParser do
+  @behaviour Parser
+
+  @impl Parser
+  def parse(str), do: {:ok, "some yaml" <> str}  # parse YAML
+
+  @impl Parser
+  def extensions, do: ["yml", "yaml"]
+end
